fea203ddad82c947c6042ff30c103b2c789c1d68
Hoang Kiem <kiemhd@outlook.com>
2022-09-22 14:57:50 +0700

    Practice with more attribute

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -4,17 +4,46 @@ export default {
     components: {
         Assignment
     },
+    template: `
+        <section v-if="assignments.length">
+            <h2 class="font-bold mb-2">
+                {{title}}
+                <span>({{ assignments.length }})</span>
+            </h2>
+
+            <div className="flex gap-2">
+                <button 
+                    @click="currentTag = tag"
+                    v-for="tag in tags" 
+                    class="border rounded px-1 py-px text-xs"
+                    :class="{'border-blue-500 text-blue-500': currentTag === tag}"
+                >{{ tag }}</button>
+            </div>
+
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+                <assignment v-for="assignment in filteredAssignments" :key="assignment.id" :assignment="assignment"></assignment>
+            </ul>
+        </section>
+    `,
     props: {
         assignments: Array,
         title: String
     },
-    template: `
-        <section v-if="assignments.length">
-            <h2 class="font-bold mb-2">{{title}}</h2>
-            <ul class="border border-gray-600 divide-y divide-gray-600">
-                <assignment v-for="assignment in assignments" :key="assignment.id" :assignment="assignment"></assignment>
-            </ul>
-        </section>
-    `,
+    data() {
+        return {
+            currentTag: 'all'
+        }
+    },
+    computed: {
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(assignment => assignment.tag === this.currentTag);
+        },
+        tags() {
+            return ['all', ...new Set( this.assignments.map(a => a.tag))];
+        }
+    }
 
 }
