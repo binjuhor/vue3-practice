a542c0b24570773f248a31c13425fe29bd6763ef
Hoang Kiem <kiemhd@outlook.com>
2022-09-22 10:51:56 +0700

    Component Props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,7 +1,13 @@
 export default {
     template: `
         <button 
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+            :class="{
+                'text-white font-bold py-2 px-4 rounded cursor-pointer relative': true,
+                'bg-blue-500 hover:bg-blue-700': type === 'primary',
+                'bg-red-500 hover:bg-red-700': type === 'danger',
+                'bg-green-500 hover:bg-green-700': type === 'success',
+                'is-loading text-transparent': processing
+            }"
             :disabled="processing"
         >
             <slot/>
@@ -10,9 +16,14 @@ export default {
     mounted() {
         console.log('Mounted');
     },
-    data() {
-        return {
-            processing: true
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        processing: {
+            type: Boolean,
+            default: false
         }
     },
 }
