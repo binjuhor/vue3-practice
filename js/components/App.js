48a2db9da0194f19dce2296d13314da300bb9859
Hoang Kiem <kiemhd@outlook.com>
2022-09-22 11:49:03 +0700

    Event Handling + Handle a Form Submitssion

--- js/components/App.js
+++ js/components/App.js
@@ -8,6 +8,13 @@ export default  {
     <div class="space-y-6">
         <assignment-list :assignments="filters.inProgress" title="In Progress" />
         <assignment-list :assignments="filters.completed" title="Completed" />
+
+        <form @submit.prevent="add">
+            <div className="border border-gray-600 text-black">
+                <input placeholder="Add Assignment"  class="p-2"  v-model="newAssignment"/>
+                <button type="submit" class="bg-white p-2 border-l">Add</button>
+            </div>
+        </form>
     </div>
     `,
     data() {
@@ -16,7 +23,8 @@ export default  {
                 {name: 'Finish Project', complete: false, id: 1},
                 {name: 'Read chapter 4', complete: false, id: 2},
                 {name: 'Turn in homework', complete: false, id: 3}
-            ]
+            ],
+            newAssignment: ''
         }
     },
     computed: {
@@ -26,5 +34,17 @@ export default  {
                 completed: this.assignments.filter(assignment => assignment.complete)
             }
         }
-    }
+    },
+    methods: {
+        add() {
+            if (this.newAssignment) {
+                this.assignments.push({
+                    name: this.newAssignment,
+                    complete: false,
+                    id: this.assignments.length + 1
+                });
+                this.newAssignment = '';
+            }
+        }
+    },
 }
--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -1,8 +1,10 @@
 export default {
     template: `
         <li>
-            <label class="cursor-pointer">{{ assignment.name }}</label>
-            <input class="ml-2" type="checkbox" v-model="assignment.complete" />
+            <label class="cursor-pointer p-2 flex justify-between items-center">{{ assignment.name }}
+            
+                <input class="ml-3 border-gray-500" type="checkbox" v-model="assignment.complete" />
+            </label>
         </li>
     `,
     props: {
