b1ba7699bb6c8b6f8510b2258f472b1f4c8d1f9f
Hoang Kiem <kiemhd@outlook.com>
2022-09-23 11:49:37 +0700

    Component Responsibility

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,8 +1,10 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
     components: {
-        Assignment
+        Assignment,
+        AssignmentTags
     },
     template: `
         <section v-if="assignments.length">
@@ -11,14 +13,11 @@ export default {
                 <span>({{ assignments.length }})</span>
             </h2>
 
-            <div className="flex gap-2">
-                <button 
-                    @click="currentTag = tag"
-                    v-for="tag in tags" 
-                    class="border rounded px-1 py-px text-xs"
-                    :class="{'border-blue-500 text-blue-500': currentTag === tag}"
-                >{{ tag }}</button>
-            </div>
+          <assignment-tags 
+            :initial-tags="assignments.map(a => a.tag)"
+            :current-tag="currentTag"
+            @change="currentTag = $event"
+        />
 
             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment v-for="assignment in filteredAssignments" :key="assignment.id" :assignment="assignment"></assignment>
@@ -40,10 +39,6 @@ export default {
                 return this.assignments;
             }
             return this.assignments.filter(assignment => assignment.tag === this.currentTag);
-        },
-        tags() {
-            return ['all', ...new Set( this.assignments.map(a => a.tag))];
         }
     }
-
 }
