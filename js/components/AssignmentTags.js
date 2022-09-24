d7098a31e2661fbdd1fcac1aa5399989bf82bb42
Hoang Kiem <kiemhd@outlook.com>
2022-09-25 05:37:41 +0700

    A Deeper look af V-Model

--- js/components/AssignmentTags.js
+++ js/components/AssignmentTags.js
@@ -2,10 +2,10 @@ export default {
     template: `
       <div className="flex gap-2">
         <button 
-            @click="$emit('change', tag)"
+            @click="$emit('update:currentTag', tag)"
             v-for="tag in tags" 
             class="border rounded px-1 py-px text-xs"
-            :class="{'border-blue-500 text-blue-500': currentTag === tag}"
+            :class="{'border-blue-500 text-blue-500': tag === currentTag }"
         >{{ tag }}</button>
     </div>`,
     props: {
